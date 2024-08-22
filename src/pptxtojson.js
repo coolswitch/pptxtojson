d360dd7aa8b7d82d6612d0111d89bd06c25b0646
戴鹏云 <daipy006@avic.com>
2024-08-22 21:47:30 +0800

    fix: 背景

--- src/pptxtojson.js
+++ src/pptxtojson.js
@@ -269,10 +269,15 @@ async function processSingleSlide(zip, sldFileName, themeContent, defaultTextSty
     defaultTextStyle,
     options,
   }
-  // const bgElements = await getBackground(warpObj)
+  const bgElements = await getBackground(warpObj)
   const bgColor = await getSlideBackgroundFill(warpObj)
 
   const elements = []
+  // if (!bgColor.backgroundType) {
+  //   bgColor.background = '#fff'
+  //   bgColor.backgroundType = 'color'
+  elements.push(...bgElements)
+  // }
   for (const nodeKey in nodes) {
     if (nodes[nodeKey].constructor === Array) {
       for (const node of nodes[nodeKey]) {
@@ -292,50 +297,50 @@ async function processSingleSlide(zip, sldFileName, themeContent, defaultTextSty
   }
 }
 
-// async function getBackground(warpObj) {
-//   const elements = []
-//   const slideLayoutContent = warpObj['slideLayoutContent']
-//   const slideMasterContent = warpObj['slideMasterContent']
-//   const nodesSldLayout = getTextByPathList(slideLayoutContent, ['p:sldLayout', 'p:cSld', 'p:spTree'])
-//   const nodesSldMaster = getTextByPathList(slideMasterContent, ['p:sldMaster', 'p:cSld', 'p:spTree'])
+async function getBackground(warpObj) {
+  const elements = []
+  const slideLayoutContent = warpObj['slideLayoutContent']
+  const slideMasterContent = warpObj['slideMasterContent']
+  const nodesSldLayout = getTextByPathList(slideLayoutContent, ['p:sldLayout', 'p:cSld', 'p:spTree'])
+  const nodesSldMaster = getTextByPathList(slideMasterContent, ['p:sldMaster', 'p:cSld', 'p:spTree'])
 
-//   const showMasterSp = getTextByPathList(slideLayoutContent, ['p:sldLayout', 'attrs', 'showMasterSp'])
-//   if (nodesSldLayout) {
-//     for (const nodeKey in nodesSldLayout) {
-//       if (nodesSldLayout[nodeKey].constructor === Array) {
-//         for (let i = 0; i < nodesSldLayout[nodeKey].length; i++) {
-//           const ph_type = getTextByPathList(nodesSldLayout[nodeKey][i], ['p:nvSpPr', 'p:nvPr', 'p:ph', 'attrs', 'type'])
-//           if (ph_type !== 'pic') {
-//             const ret = await processNodesInSlide(nodeKey, nodesSldLayout[nodeKey][i], warpObj, 'slideLayoutBg')
-//             if (ret) elements.push(ret)
-//           }
-//         }
-//       } 
-//       else {
-//         const ph_type = getTextByPathList(nodesSldLayout[nodeKey], ['p:nvSpPr', 'p:nvPr', 'p:ph', 'attrs', 'type'])
-//         if (ph_type !== 'pic') {
-//           const ret = await processNodesInSlide(nodeKey, nodesSldLayout[nodeKey], warpObj, 'slideLayoutBg')
-//           if (ret) elements.push(ret)
-//         }
-//       }
-//     }
-//   }
-//   if (nodesSldMaster && (showMasterSp === '1' || showMasterSp)) {
-//     for (const nodeKey in nodesSldMaster) {
-//       if (nodesSldMaster[nodeKey].constructor === Array) {
-//         for (let i = 0; i < nodesSldMaster[nodeKey].length; i++) {
-//           const ret = await processNodesInSlide(nodeKey, nodesSldMaster[nodeKey][i], warpObj, 'slideMasterBg')
-//           if (ret) elements.push(ret)
-//         }
-//       } 
-//       else {
-//         const ret = await processNodesInSlide(nodeKey, nodesSldMaster[nodeKey], warpObj, 'slideMasterBg')
-//         if (ret) elements.push(ret)
-//       }
-//     }
-//   }
-//   return elements
-// }
+  const showMasterSp = getTextByPathList(slideLayoutContent, ['p:sldLayout', 'attrs', 'showMasterSp'])
+  if (nodesSldLayout) {
+    for (const nodeKey in nodesSldLayout) {
+      if (nodesSldLayout[nodeKey].constructor === Array) {
+        for (let i = 0; i < nodesSldLayout[nodeKey].length; i++) {
+          const ph_type = getTextByPathList(nodesSldLayout[nodeKey][i], ['p:nvSpPr', 'p:nvPr', 'p:ph', 'attrs', 'type'])
+          if (ph_type !== 'pic') {
+            const ret = await processNodesInSlide(nodeKey, nodesSldLayout[nodeKey][i], warpObj, 'slideLayoutBg')
+            if (ret && ret.type !== 'text') elements.push(ret)
+          }
+        }
+      } 
+      else {
+        const ph_type = getTextByPathList(nodesSldLayout[nodeKey], ['p:nvSpPr', 'p:nvPr', 'p:ph', 'attrs', 'type'])
+        if (ph_type !== 'pic') {
+          const ret = await processNodesInSlide(nodeKey, nodesSldLayout[nodeKey], warpObj, 'slideLayoutBg')
+          if (ret && ret.type !== 'text') elements.push(ret)
+        }
+      }
+    }
+  }
+  if (nodesSldMaster && (showMasterSp === '1' || showMasterSp)) {
+    for (const nodeKey in nodesSldMaster) {
+      if (nodesSldMaster[nodeKey].constructor === Array) {
+        for (let i = 0; i < nodesSldMaster[nodeKey].length; i++) {
+          const ret = await processNodesInSlide(nodeKey, nodesSldMaster[nodeKey][i], warpObj, 'slideMasterBg')
+          if (ret && ret.type !== 'text') elements.push(ret)
+        }
+      } 
+      else {
+        const ret = await processNodesInSlide(nodeKey, nodesSldMaster[nodeKey], warpObj, 'slideMasterBg')
+        if (ret && ret.type !== 'text') elements.push(ret)
+      }
+    }
+  }
+  return elements
+}
 
 function indexNodes(content) {
   const keys = Object.keys(content)
